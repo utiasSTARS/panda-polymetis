8df4d071aae4daa25edc9ad9dc612aaa9041483a
Trevor Ablett <trevor.ablett@gmail.com>
2024-05-19 19:50:14 -0400

    For new rl env, remove unnecessary file

--- panda_polymetis/tools/get_joint_pos.py
+++ panda_polymetis/tools/get_joint_pos.py
@@ -1,20 +1,30 @@
 import os
-# from polymetis import RobotInterface
+import argparse
+
 import numpy as np
 import panda_polymetis
 from panda_polymetis.control.panda_client import PandaClient
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--sim', action='store_true')
+args = parser.parse_args()
+
+
+if args.sim:
+    server_ip = 'localhost'
+else:
+    server_ip = os.environ.get('NUC_IP', '192.168.2.2')
 
 pp_dir = os.path.dirname(panda_polymetis.__file__)
 json_file = os.path.join(pp_dir, 'conf/franka-desk/franka-gripper-and-blue-realsense.json')
-pc = PandaClient(server_ip=os.environ['NUC_IP'], ee_config_json=json_file)
+pc = PandaClient(server_ip=server_ip, ee_config_json=json_file)
 state = pc.get_and_update_state()
 num_dec = 7
 
 base_tool_tf = state['EE_pose'].get_array_euler(axes='sxyz')
 bttf_beginning = ', '.join([f"{e:.{num_dec}}" for e in base_tool_tf[:-1]])
-print(f"reset_base_tool_tf: [{bttf_beginning}, {base_tool_tf[-1]:.{num_dec}}]")
+print(f"reset_pose: [{bttf_beginning}, {base_tool_tf[-1]:.{num_dec}}]")
 
 joint_pos = np.array(state['joint_position'])
 jp_beginning = ', '.join([f"{e:.{num_dec}}" for e in joint_pos[:-1]])
-print(f"reset_joint_pos: [{jp_beginning}, {joint_pos[-1]:.{num_dec}}]")
+print(f"reset_joints: [{jp_beginning}, {joint_pos[-1]:.{num_dec}}]")
